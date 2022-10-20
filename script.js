903950c4a93fa43a517a72d1a47aeda95c493de0
Kenny Hung (he) <khung@instructors.2u.com>
2022-10-20 18:55:24 +0100

    Set up test cases code

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('Hello')
+function isPalindrome(word) {
+  return null
+}
+
+console.log('Expect true')
+console.log(isPalindrome('noon'))
+
+console.log('Expect false')
+console.log(isPalindrome('horse'))
+
+console.log('Expect true')
+console.log(isPalindrome('racecar'))
+
+/*
+Ex:
+Input: "noon"
+Output: true
+
+Input: "horse"
+Output: false
+
+Input: "racecar"
+Output: true
+*/
